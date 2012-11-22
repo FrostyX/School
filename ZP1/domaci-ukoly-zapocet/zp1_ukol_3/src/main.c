faf58427ae59d11f8f8c8bf1cfc739e5cee955c4
Jakub Kadlčík <frostyx@email.cz>
2012-11-22 19:02:26 +0100

    Napsána funkce pro přidávání osob

--- ZP1/domaci-ukoly-zapocet/zp1_ukol_3/src/main.c
+++ ZP1/domaci-ukoly-zapocet/zp1_ukol_3/src/main.c
@@ -12,6 +12,15 @@
 
 typedef struct
 {
+	char *jmeno;
+	char *prijmeni;
+	char *adresa;
+	char den;
+	char mesic;
+	int rok;
+	char *telefon;
+	char *email;
+	/*
 	char jmeno[10];
 	char prijmeni[15];
 	char adresa[20];
@@ -20,6 +29,7 @@ typedef struct
 	int rok;
 	char telefon[16];
 	char email[15];
+	*/
 } osoba;
 
 int vytvor_osobu(char *jmeno, char *prijmeni, char *adresa, char den, char mesic, int rok, char *telefon, char *email, osoba *seznam_osob);
@@ -29,12 +39,28 @@ int tisk(osoba *seznam_osob);
 
 int main(int argc, char **argv)
 {
+	// Vytvoření dynamického pole pro ukládání prvků datového typu osoba
+	osoba *osoby;
+
+	// Použití funkcí pro práci s osobami
+	vytvor_osobu("Tony", "Stark", "New York City", 2, 3, 1963, "987 654 321", "ironman@foo.com", osoby);
+	//vytvor_osobu("Bruce", "Wayne", "Gotham City", 1, 5, 1939, "123 456 789", "batman@foo.com", osoby);
 	return 0;
 }
 
 int vytvor_osobu(char *jmeno, char *prijmeni, char *adresa, char den, char mesic, int rok, char *telefon, char *email, osoba *seznam_osob)
 {
-	return 0;
+	// Vytvoření datové struktury z předaných parametrů
+	osoba novaOsoba = {jmeno, prijmeni, adresa, den, mesic, rok, telefon, email};
+
+	// Zvětšení pole, aby se do něj vešel další prvek datového typu osoba
+	int n = sizeof(seznam_osob)/sizeof(osoba); // Počet prvků v poli
+	seznam_osob = (osoba*)malloc(sizeof(seznam_osob)+sizeof(osoba));
+
+	// Přidání nové osoby do předaného pole
+	seznam_osob[n] = novaOsoba;
+
+	return n+1;
 }
 
 int zrus_osobu(char *celejmeno, osoba *seznam)
