2068d61512ee521eb37fb9e5fc107b97dfe7efac
Jakub Kadlčík <frostyx@email.cz>
2012-11-30 02:12:33 +0100

    Přidáno řešení třetího příkladu

--- PAPR1/bartl/ukol-2/ukol-2.rkt
+++ PAPR1/bartl/ukol-2/ukol-2.rkt
@@ -19,3 +19,22 @@
 (define switch
 	(lambda (par)
 		(my-cons (my-cdr par) (my-car par))))
+
+; 3
+(define make-c cons)
+(define real car)
+(define imag cdr)
+
+(define conj
+	(lambda (par)
+		(cons (car par) (- (cdr par)))))
+
+(define c+
+	(lambda (a b)
+		(make-c (+ (real a) (real b)) (+ (imag a) (imag b)))))
+
+(define c*
+	(lambda (a b)
+		(make-c
+			(- (* (real a) (real b)) (* (imag a) (imag b)))
+			(+ (* (real a) (imag b)) (* (imag a) (real b))))))
