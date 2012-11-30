1f129d263b54ecab3560d38e94e6838306d7b2f9
Jakub Kadlčík <frostyx@email.cz>
2012-11-30 01:10:04 +0100

    Přidáno řešení prvního příkladu

--- PAPR1/bartl/ukol-2/ukol-2.rkt
+++ PAPR1/bartl/ukol-2/ukol-2.rkt
@@ -1,3 +1,16 @@
 #!/usr/bin/racket
 #lang racket
 
+; 1
+(define my-cons
+	(lambda (a b)
+		(lambda (predikat)
+			(if predikat a b))))
+
+(define my-car
+	(lambda (par)
+		(par #t)))
+
+(define my-cdr
+	(lambda (par)
+		(par #f)))
