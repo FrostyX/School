e72872c4e613fe1abe22a57242ea35f936893812
Jakub Kadlčík <frostyx@email.cz>
2012-12-11 10:28:08 +0100

    Přidána procedura approx=

---
+++ PAPR1/implementace-sys-procedur/approx-eq.rkt
@@ -0,0 +1,12 @@
+#!/usr/bin/racket
+#lang racket
+
+(define approx=
+	(lambda (a b . x)
+		(let ((x (if (null? x)
+				1/1000 ; Předdefinovaná odchylka
+				(car x)))) ; car, protože x je z nějakého dúvodu seznam
+		(<= (abs (- a b)) x))))
+
+(approx= 5 5.00001 1/10)
+(approx= 5 5.10)
