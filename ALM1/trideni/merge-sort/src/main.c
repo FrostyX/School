bde584e76237495363d234de6bcb46e3feba156b
Jakub Kadlčík <frostyx@email.cz>
2012-12-29 15:12:54 +0100

    Presun do slozky pro trideni

---
+++ ALM1/trideni/merge-sort/src/main.c
@@ -0,0 +1,24 @@
+/* Zadání:
+ *
+ * Jakub Kadlčík [jakub.kadlcik01@upol.cz]
+ *
+ * Product is under The BSD 3-Clause License
+ * Copyright (c) 2012, Jakub Kadlčík
+ * All rights reserved.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	int cisla[] = {8, 9, 7, 3, 2, 6, 10, 5};
+	int n = sizeof(cisla)/sizeof(int);
+	mergesort(cisla, 0, n-1);
+	return 0;
+}
+
+int mergesort(int a[], int zacatek, int konec)
+{
+
+}
