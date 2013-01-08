253ea005199080b127f2fd5bed086143fa2aa75f
Jakub Kadlčík <frostyx@email.cz>
2013-01-08 18:48:59 +0100

    Přidána šablona pro counting-sort

---
+++ ALM1/trideni/counting-sort/src/main.c
@@ -0,0 +1,16 @@
+/* Zadání:
+ *
+ * Jakub Kadlčík [jakub.kadlcik01@upol.cz]
+ *
+ * Product is under The BSD 3-Clause License
+ * Copyright (c) 2012, Jakub Kadlčík
+ * All rights reserved.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
