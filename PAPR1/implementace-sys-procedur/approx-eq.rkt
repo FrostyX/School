a2a4dfad4ef95cdd246dfe2f7e8c8577469eece0
Jakub Kadlčík <frostyx@email.cz>
2013-01-30 00:17:08 +0100

    Vysvětlen důvod car pro nepoviné argumenty

--- PAPR1/implementace-sys-procedur/approx-eq.rkt
+++ PAPR1/implementace-sys-procedur/approx-eq.rkt
@@ -5,7 +5,7 @@
 	(lambda (a b . x)
 		(let ((x (if (null? x)
 				1/1000 ; Předdefinovaná odchylka
-				(car x)))) ; car, protože x je z nějakého dúvodu seznam
+				(car x)))) ; car, protože x je seznam obsahující nepoviné argumenty
 		(<= (abs (- a b)) x))))
 
 (approx= 5 5.00001 1/10)
