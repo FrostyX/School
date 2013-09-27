d5236f62ab3f2a78e6db0fb80d30e6ddff609b63
Jakub Kadlčík <frostyx@email.cz>
2013-09-27 11:47:45 +0200

    Přidán první bodovaný úkol (1/2)

---
+++ ZP3CS/bodovane-ukoly/1-konzole-vypis/1-konzole-vypis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1_konzole_vypis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jakub Kadlčík");
+            Console.WriteLine("Jeseník");
+            Console.WriteLine("79001");
+        }
+    }
+}
