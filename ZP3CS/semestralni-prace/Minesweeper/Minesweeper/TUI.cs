21959ce04906fa76afdb1ef1456a6422af2b1c9e
Jakub Kadlčík <frostyx@email.cz>
2013-09-28 16:37:06 +0200

    Přidána třída 'TUI' pro práci s textovým rozhraním

---
+++ ZP3CS/semestralni-prace/Minesweeper/Minesweeper/TUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minesweeper
+{
+	class TUI : UI
+	{
+		public void printGrid(int n, int[] values = null)
+		{
+		}
+
+		public void printStats(Stats s)
+		{
+		}
+	}
+}
