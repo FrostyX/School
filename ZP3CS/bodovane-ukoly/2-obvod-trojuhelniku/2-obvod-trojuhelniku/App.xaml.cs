22a1bc7034c195355192c145881994545d09fd68
Jakub Kadlčík <frostyx@email.cz>
2013-10-01 07:53:26 +0200

    Přidán druhý bodovaný úkol

---
+++ ZP3CS/bodovane-ukoly/2-obvod-trojuhelniku/2-obvod-trojuhelniku/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace _2_obvod_trojuhelniku
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
---
+++ ZP3CS/bodovane-ukoly/2-obvod-trojuhelniku/2-obvod-trojuhelniku/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2_obvod_trojuhelniku
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		protected bool lzeSestrojitTrojuhelnik(int a, int b, int c)
+		{
+			if (a + b < c) return false;
+			if (a + c < b) return false;
+			if (b + c < a) return false;
+			return true;
+		}
+
+		protected int obvodTrojuhelnika(int a, int b, int c)
+		{
+			return a + b + c;
+		}
+
+		private void bVypocti_Click(object sender, RoutedEventArgs e)
+		{
+			int a = Convert.ToInt32(tA.Text);
+			int b = Convert.ToInt32(tB.Text);
+			int c = Convert.ToInt32(tC.Text);
+			bool lze = this.lzeSestrojitTrojuhelnik(a, b, c);
+
+			if (lze)
+			{
+				lVysledek.Content = "Trojúhelník lze sestrojit\n";
+				lVysledek.Content += "Obvod: " + this.obvodTrojuhelnika(a, b, c).ToString();
+			}
+			else
+			{
+				lVysledek.Content = "Trojúhelník nelze sestrojit";
+			}
+		}
+	}
+}
