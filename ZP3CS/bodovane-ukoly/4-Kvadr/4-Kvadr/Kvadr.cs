210e13044c365d11fe3cbcb7b0859bd270d0e8e4
Jakub Kadlčík <frostyx@email.cz>
2013-10-15 20:39:14 +0200

    Přidán čtvrtý bodovaný úkol

---
+++ ZP3CS/bodovane-ukoly/4-Kvadr/4-Kvadr/Kvadr.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _4_Kvadr
+{
+	class Kvadr
+	{
+		public double a;
+		public double b;
+		public double c;
+
+		public Kvadr(double a=1, double b=1, double c=1)
+		{
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+
+		public double vratObjem()
+		{
+			return a*b*c;
+		}
+
+		public double vratPovrch()
+		{
+			return 2*(a*b + b*c + a*c);
+		}
+
+	}
+}
---
+++ ZP3CS/bodovane-ukoly/4-Kvadr/4-Kvadr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _4_Kvadr
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			vypisKvadr(new Kvadr());
+			vypisKvadr(new Kvadr(4));
+			vypisKvadr(new Kvadr(4, 5));
+			vypisKvadr(new Kvadr(4, 5, 6));
+			Console.ReadKey();
+		}
+
+		static void vypisKvadr(Kvadr k)
+		{
+			Console.WriteLine(
+				"Kvádr byl zadán stranami a={0}; b={1}; c={2} -> Objem: {3}; Povrch: {4}",
+				k.a, k.b, k.c, k.vratObjem(), k.vratPovrch()
+			);
+		}
+	}
+}
