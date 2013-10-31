3b9effc2ba6ad4ef00910dd7da203ba5d0ba0fd3
Jakub Kadlčík <frostyx@email.cz>
2013-10-31 18:02:43 +0100

    Dynamická velikost políčka

--- OOP/domaci-ukoly/6-piskvorky/6-piskvorky.lisp
+++ OOP/domaci-ukoly/6-piskvorky/6-piskvorky.lisp
@@ -1,5 +1,11 @@
 (load (current-pathname "micro-graphics.lisp"))
 
+; Konfigurace
+(defvar *field-width* 50)
+(defvar *fields-in-row* 3)
+(defvar *gap-x* 45)
+(defvar *gap-y* 40)
+
 ;; FIELD
 (defclass field (picture) 
   ((value :initform 0))) ; Value: -1 0 1
@@ -14,9 +20,9 @@
 (defmethod border ((field field))
   (let ((border (make-instance 'polygon))
         (A (make-point 0 0))
-        (B (make-point 0 50))
-        (C (make-point 50 50))
-        (D (make-point 50 0)))
+        (B (make-point 0 *field-width*))
+        (C (make-point *field-width* *field-width*))
+        (D (make-point *field-width* 0)))
 
     (set-items border (list A B C D))
     border))
@@ -26,11 +32,14 @@
   (let* ((cross (make-instance 'picture))
          (l1 (make-instance 'polygon))
          (l2 (make-instance 'polygon)))
-    
-    (set-thickness l1 5)
-    (set-items l1 (list (make-point 10 10) (make-point 40 40)))
-    (set-thickness l2 5)
-    (set-items l2 (list (make-point 10 40) (make-point 40 10)))
+
+    (let* ((a (/ *field-width* 5))
+           (b (- *field-width* a)))
+      (set-items l1 (list (make-point a a) (make-point b b)))
+      (set-items l2 (list (make-point a b) (make-point b a)))
+      
+      (set-thickness l1 a)
+      (set-thickness l2 a))
 
     (set-color cross :red)
     (set-propagate-color-p cross t)
@@ -39,12 +48,14 @@
 
 ; Vrátí objekt reprezentující značku druhého hráče - kolečko
 (defmethod circle ((field field))
-  (let ((c (make-instance 'circle))
-        (s (make-point 25 25)))
+  (let* ((c (make-instance 'circle))
+         (half (/ *field-width* 2))
+         (tenth (/ *field-width* 10))
+         (s (make-point half half)))
 
     (set-center c s)
-    (set-radius c 15)
-    (set-thickness c 5)
+    (set-radius c (/ (+ half tenth) 2))
+    (set-thickness c tenth)
     (set-color c :green)
     c))
 
@@ -73,20 +84,26 @@
 (setf *win* (make-instance 'window))
 
 (let ((p (make-instance 'picture))
-      (f1 (make-field -1))
-      (f2 (make-field 0))
-      (f3 (make-field 1))
-      (f4 (make-field 1))
-      (f5 (make-field -1))
-      (f6 (make-field 0)))
+      (fields (list (make-field -1)
+                    (make-field 0)
+                    (make-field 1)
+                    (make-field 1)
+                    (make-field -1)
+                    (make-field 0)))
+      (x 0)
+      (y 0))
 
-  (move f1 50 50)
-  (move f2 100 50)
-  (move f3 150 50)
-  (move f4 50 100)
-  (move f5 100 100)
-  (move f6 150 100)
+  (dolist (field fields)
+    (move field 
+          (+ (* *field-width* x) *gap-x*) 
+          (+ (* *field-width* y) *gap-y*))
 
-  (set-items p (list f1 f2 f3 f4 f5 f6))
+    (setq x (+ x 1))
+    (if (= x *fields-in-row*)
+        (progn 
+          (setq x 0)
+          (setq y (+ y 1)))))
+          
+  (set-items p fields)
   (set-shape *win* p)
   (redraw *win*))
