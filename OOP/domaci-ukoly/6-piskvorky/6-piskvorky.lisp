93f999e20a1a513d3ec5676752054295bcc401d3
Jakub Kadlčík <frostyx@email.cz>
2013-10-31 18:11:43 +0100

    metody -> lokální funkce

--- OOP/domaci-ukoly/6-piskvorky/6-piskvorky.lisp
+++ OOP/domaci-ukoly/6-piskvorky/6-piskvorky.lisp
@@ -16,64 +16,65 @@
     (set-value field value)
     field))
 
-; Vrátí objekt reprezentující rámeček okolo políčka
-(defmethod border ((field field))
-  (let ((border (make-instance 'polygon))
-        (A (make-point 0 0))
-        (B (make-point 0 *field-width*))
-        (C (make-point *field-width* *field-width*))
-        (D (make-point *field-width* 0)))
-
-    (set-items border (list A B C D))
-    border))
-
-; Vrátí objekt reprezentující značku prvního hráče - křížek
-(defmethod cross ((field field))
-  (let* ((cross (make-instance 'picture))
-         (l1 (make-instance 'polygon))
-         (l2 (make-instance 'polygon)))
-
-    (let* ((a (/ *field-width* 5))
-           (b (- *field-width* a)))
-      (set-items l1 (list (make-point a a) (make-point b b)))
-      (set-items l2 (list (make-point a b) (make-point b a)))
-      
-      (set-thickness l1 a)
-      (set-thickness l2 a))
-
-    (set-color cross :red)
-    (set-propagate-color-p cross t)
-    (set-items cross (list l1 l2))
-    cross))
-
-; Vrátí objekt reprezentující značku druhého hráče - kolečko
-(defmethod circle ((field field))
-  (let* ((c (make-instance 'circle))
-         (half (/ *field-width* 2))
-         (tenth (/ *field-width* 10))
-         (s (make-point half half)))
-
-    (set-center c s)
-    (set-radius c (/ (+ half tenth) 2))
-    (set-thickness c tenth)
-    (set-color c :green)
-    c))
-
 ; Gettery & Settery
 (defmethod value ((field field))
   (slot-value field 'value))
 
 (defmethod set-value ((field field) value) 
+  (labels 
+      ; Vrátí objekt reprezentující rámeček okolo políčka
+      ((border ()
+         (let ((border (make-instance 'polygon))
+               (A (make-point 0 0))
+               (B (make-point 0 *field-width*))
+               (C (make-point *field-width* *field-width*))
+               (D (make-point *field-width* 0)))
+
+           (set-items border (list A B C D))
+           border))
+
+       ; Vrátí objekt reprezentující značku prvního hráče - křížek
+       (cross ()
+         (let* ((cross (make-instance 'picture))
+                (l1 (make-instance 'polygon))
+                (l2 (make-instance 'polygon)))
+
+           (let* ((a (/ *field-width* 5))
+                  (b (- *field-width* a)))
+             (set-items l1 (list (make-point a a) (make-point b b)))
+             (set-items l2 (list (make-point a b) (make-point b a)))
+      
+             (set-thickness l1 a)
+             (set-thickness l2 a))
+
+           (set-color cross :red)
+           (set-propagate-color-p cross t)
+           (set-items cross (list l1 l2))
+           cross))
+
+       ; Vrátí objekt reprezentující značku druhého hráče - kolečko
+       (circle ()
+         (let* ((c (make-instance 'circle))
+                (half (/ *field-width* 2))
+                (tenth (/ *field-width* 10))
+                (s (make-point half half)))
+
+           (set-center c s)
+           (set-radius c (/ (+ half tenth) 2))
+           (set-thickness c tenth)
+           (set-color c :green)
+           c)))
+
   (unless (and (>= value -1) (<= value 1))
     (error "Hodnota policka musi byt -1, 0, nebo 1"))
   
-  (let ((value-image (cond ((= value -1) (cross field))
-                           ((= value +1) (circle field))
+  (let ((value-image (cond ((= value -1) (cross))
+                           ((= value +1) (circle))
                            (t (make-instance 'empty-shape)))))
 
-    (set-items field (list value-image (border field)))
+    (set-items field (list value-image (border)))
     (setf (slot-value field 'value) value)
-    field))
+    field)))
 
 
 
