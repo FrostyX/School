d11462bfdc401da9c16be3b14d4114673969bd46
Jakub Kadlčík <frostyx@email.cz>
2013-11-19 17:48:10 +0100

    Přidán devátý bodovaný úkol

---
+++ ZP3CS/bodovane-ukoly/9-mnozina/9-mnozina/Mnozina.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace _9_mnozina
+{
+	class Mnozina
+	{
+		private ArrayList seznam = null;
+
+		public Mnozina(ArrayList seznam = null)
+		{
+			if (seznam == null)
+				seznam = new ArrayList();
+			this.seznam = seznam;
+		}
+
+		public static Mnozina operator >> (Mnozina m, int c)
+		{
+			m.seznam.Remove(c);
+			return m;
+		}
+
+		public static Mnozina operator << (Mnozina m, int c)
+		{
+			m.seznam.Add(c);
+			return m;
+		}
+
+		public static bool operator ! (Mnozina m)
+		{
+			return m.seznam.Count <= 0;
+		}
+
+		public static int operator + (Mnozina m)
+		{
+			return m.seznam.Count;
+		}
+
+		public static Mnozina operator + (Mnozina a, Mnozina b)
+		{
+			ArrayList c = new ArrayList();
+			c.AddRange(a.seznam);
+			c.AddRange(b.seznam);
+			return new Mnozina(c);
+		}
+
+		public static Mnozina operator - (Mnozina a, Mnozina b)
+		{
+			ArrayList rozdil = new ArrayList();
+			rozdil.AddRange(a.seznam);
+			foreach(int x in b.seznam)
+			{
+				rozdil.Remove(x);
+			}
+			return new Mnozina(rozdil);
+		}
+
+		public override string ToString()
+		{
+			string s = "[ ";
+			foreach (int x in this.seznam)
+				s += x + " ";
+			s += "]";
+			return s;
+		}
+	}
+}
---
+++ ZP3CS/bodovane-ukoly/9-mnozina/9-mnozina/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _9_mnozina
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Mnozina m = new Mnozina();
+			Mnozina n = new Mnozina();
+
+			m = m << 3;
+			m = m << 4;
+			m = m << 2;
+			m = m << 5;
+			m = m >> 4;
+
+			n = n << 7;
+			n = n << 4;
+			n = n << 3;
+			n = n << 5;
+
+			Console.WriteLine("!m = {0}", !m);
+			Console.WriteLine("+m = {0}", +m);
+			Console.WriteLine("m + n = {0}", m + n);
+			Console.WriteLine("m - n = {0}", m - n);
+			Console.ReadKey();
+		}
+	}
+}
