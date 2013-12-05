ee9eaa8f69d9f64ff0824e73be9df9395f91efe7
Jakub Kadlčík <frostyx@email.cz>
2013-12-05 22:31:24 +0100

    Přidán programovací úkol resolveip

---
+++ POS/programovani/8-resolveip/README.md
@@ -0,0 +1,18 @@
+## BUILD
+	cd build
+	cmake .
+	make
+## RUN
+	bin/8-resolveip
+
+## AUTHOR
+	Name: Jakub Kadlčík
+	Email: jakub.kadlcik01@upol.cz
+
+## OUTPUT
+	Hostname: google.cz
+
+	Aliases:
+	173.194.112.87 -> fra07s29-in-f23.1e100.net
+	173.194.112.95 -> fra07s29-in-f31.1e100.net
+	173.194.112.88 -> fra07s29-in-f24.1e100.net
---
+++ POS/programovani/8-resolveip/src/main.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <iostream>
+#include <arpa/inet.h>
+
+using namespace std;
+
+int main (int argc, char * const argv[]) {
+	if (argc < 0)
+	{
+		cout << "syntax: 8-resolveip google.com" << endl;
+		return 1;
+	}
+
+	for(int i = 1; i<argc; i++)
+	{
+		struct hostent *h;
+		h = NULL;
+		if ((h = gethostbyname(argv[i])) == NULL)
+		{
+			cout << "Host not found" << endl;
+			continue;
+		}
+
+		char **alternativy = h->h_aliases;
+
+		cout << "Hostname: " << h->h_name << endl << endl;
+		cout <<"Aliases: " << endl;
+
+		int j = 0;
+		while (h->h_addr_list[j] != NULL)
+		{
+			struct hostent *hs;
+			hs = gethostbyaddr(h->h_addr_list[j], sizeof(h->h_addr_list[j]), AF_INET);
+			cout << inet_ntoa(*(in_addr *)h->h_addr_list[j]) << " -> " << hs->h_name  << endl;
+			j++;
+		}
+	}
+	return 0;
+}
