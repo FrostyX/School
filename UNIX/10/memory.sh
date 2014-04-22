fe1468dfe9216d1fe618a6e2916a123ffe1ea667
Jakub Kadlčík <frostyx@email.cz>
2014-04-22 16:10:57 +0200

    Přidán úkol z desátého cvičení

---
+++ UNIX/10/memory.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+declare -A memory
+
+# Find memory usage for every user
+while read line; do
+	user=`echo $line |awk '{print $1}'`
+	mem=`echo $line |awk '{print $6}'`
+
+	cur=${memory[$user]}
+	if [ "$cur" == "" ]; then
+		cur=0
+	fi
+
+	memory[$user]=$(($cur + $mem))
+done < <(ps aux)
+
+# Print it
+for user in "${!memory[@]}"; do
+	echo $user": "${memory[$user]}
+done
