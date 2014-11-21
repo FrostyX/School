58d049e2da14d5cc7baa4e7ab7a07ed87d9d0000
Jakub Kadlčík <frostyx@email.cz>
2014-11-22 00:48:58 +0100

    Přidán balíček foo

---
+++ PRCL/bracket-packages/foo.lisp
@@ -0,0 +1,24 @@
+(defvar *pkg*)
+(setf *pkg* (list
+
+             :name "foo"
+             :version 1.0
+             :description "This is the foo package"
+             :homepage "http://foo.example.com"
+             :license "GPLv2"
+
+             :files (list
+                     "/etc/foo/foo.conf"
+                     "/usr/bin/foo"
+                     "/usr/share/foo/README.md")
+
+             :prepare (lambda ()
+                        nil)
+
+             :install (lambda ()
+                        nil)
+
+             :remove (lambda ()
+                       nil)
+
+))
